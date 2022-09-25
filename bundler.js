bb623c94766d842547b0b6ba0d9d13d965717f8c
youchann <yutaro_hayashi@voyagegroup.com>
2022-09-25 12:09:10 +0900

    display module as code

--- bundler.js
+++ bundler.js
@@ -1,8 +1,30 @@
 const fs = require('fs');
+const babylon = require('babylon');
+const traverse = require('babel-traverse').default;
+
+let ID = 0;
 
 function createAsset(filename) {
   const content = fs.readFileSync(filename, 'utf-8');
-  console.log(content);
+  const ast = babylon.parse(content, { sourceType: 'module' });
+
+  const dependencies = [];
+
+  traverse(ast, {
+    ImportDeclaration: ({ node }) => {
+      dependencies.push(node.source.value);
+    }
+  })
+
+  const id = ID++;
+
+  return {
+    id,
+    filename,
+    dependencies,
+  }
 }
 
-createAsset('./example/entry.js');
+const mainAsset = createAsset('./example/entry.js');
+
+console.log(mainAsset);
