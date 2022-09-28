86a67e86d183d2bd14041adf643f7ac0c1dcd776
Yutaro Hayashi <yutaro50@gmail.com>
2022-09-28 23:16:33 +0900

    できた

--- bundler.js
+++ bundler.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const babylon = require('babylon');
 const traverse = require('babel-traverse').default;
+const babel = require('babel-core');
 
 let ID = 0;
 
@@ -19,10 +20,15 @@ function createAsset(filename) {
 
   const id = ID++;
 
+  const { code } = babel.transformFromAst(ast, null, {
+    presets: ['env'],
+  });
+
   return {
     id,
     filename,
     dependencies,
+    code,
   }
 }
 
@@ -49,6 +55,42 @@ function createGraph(entry) {
   return queue;
 }
 
-const graph = createGraph('./example/entry.js')
+function bundle(graph) {
+  let modules = '';
 
-console.log(graph);
+  graph.forEach(mod => {
+    modules += `${mod.id}: [
+      function (require, module, exports) {
+        ${mod.code}
+      },
+      ${JSON.stringify(mod.mapping)},
+    ],`;
+  });
+
+  const result = `
+    (function(modules) {
+      function require(id) {
+        const [fn, mapping] = modules[id];
+
+        function localRequire(relativePath) {
+          return require(mapping[relativePath]);
+        }
+
+        const module = { exports: {} };
+
+        fn(localRequire, module, module.exports);
+
+        return module.exports;  
+      }
+
+      require(0);
+    })({${modules}})
+  `;
+
+  return result;
+}
+
+const graph = createGraph('./example/entry.js');
+const result = bundle(graph);
+
+console.log(result);
